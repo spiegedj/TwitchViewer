a80a55b8d21fb89c6241832a80b43786a2eff4a3
David Spiegel <spiegedj@gmail.com>
2015-08-23 10:42:27 -0500

    Added Threading

--- TwitchViewer.py
+++ TwitchViewer.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 
+import errno
 import urllib
 import urllib2
 import sys
-import os 
+import os, tempfile
 import json        
 import subprocess
+import threading
 import signal
+import time
+import datetime
+from LivestreamerPlayer import LivestreamerPlayer
+from livestreamer import Livestreamer
 
 #------- GLOBALS -----------
 twitchApiUrl = 'https://api.twitch.tv/kraken/'
@@ -14,17 +20,28 @@ clientID = 'e5yp1mbb10ju6dmag1irayg4ncybz5j'
 accessToken = 'a7vx7pwxfhiidyn7zmup202fuxgr3k'
 pageLimit = 30
 pipe = open('/dev/input/js0', 'r')
+ACCEPTABLE_ERRNO = (errno.EPIPE, errno.EINVAL, errno.ECONNRESET)
+livestreamer = Livestreamer()
+stopLSThread = False;
+pipeStreamPath = '/tmp/stream'
+lrThreads = None
+
+#-------- GLOBAL ROWS ---------
+topStreams = []
+topGames = []
+myGames = []
+myFollows = []
+streamsForGame = []
 
 class Color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
+    PURPLE = '\033[0;35m'
+    CYAN = '\033[1;36m'
+    BLUE = '\033[1;34m'
+    GREEN = '\033[1;32m'
+    YELLOW = '\033[1;33m'
+    RED = '\033[1;31m'
     UNDERLINE = '\033[4m'
+    BOLD = '\033[1m'
     END = '\033[0m'
 
 class Input:
@@ -83,6 +100,8 @@ def ReadInput():
 
                 return (action, button)
 
+
+# Calls the twitch API. Method and params are passed in.
 def TwitchCall(method, params):
     try:
         data = urllib.urlencode(params)
@@ -118,13 +137,11 @@ def SafeJSONAccess(node, subnode):
     return ''
 
 def GetGames(pageNumber, y):
-    Clear()
-    print "loading..."
     method = 'games/top'
     offset = pageNumber * pageLimit
     params = {'offset' : offset, 'limit': pageLimit}
     gamesDict = TwitchCall(method, params)
-    count = len(gamesDict['top'])
+    count = min(len(gamesDict['top']), pageLimit)
 
     games = []
     for i in range(count):
@@ -141,7 +158,6 @@ def GetGames(pageNumber, y):
     return games
 
 def GetMyGames(pageNumber, y):
-    Clear();
     games = []
     games.append(['Starcraft II'])
     games.append(['Kerbal Space Program'])
@@ -153,13 +169,11 @@ def GetMyGames(pageNumber, y):
     
 
 def GetFollowedStreams(pageNumber, y):
-    Clear()
-    print "loading..."
     method = 'streams/followed'
     offset = pageNumber * pageLimit
     params = {'offset': offset, 'limit': pageLimit, 'oauth_token' : accessToken}
     streamsDict = TwitchCall(method, params)
-    count = len(streamsDict['streams'])
+    count = min(len(streamsDict['streams']), pageLimit)
 
     streams = []
     for i in range(count):
@@ -177,13 +191,11 @@ def GetFollowedStreams(pageNumber, y):
     return streams
 
 def GetTopStreams(pageNumber, y):
-    Clear()
-    print "loading..."
     method = 'streams'
     offset = pageNumber * pageLimit
     params = {'offset': offset, 'limit': pageLimit}
     streamsDict = TwitchCall(method, params)
-    count = len(streamsDict['streams'])
+    count = min(len(streamsDict['streams']), pageLimit)
 
     streams = []
     for i in range(count):
@@ -201,13 +213,11 @@ def GetTopStreams(pageNumber, y):
     return streams
 
 def GetStreams(pageNumber, game):
-    Clear()
-    print "loading..."
     method = 'streams'
     offset = pageNumber * pageLimit
     params = {'offset': offset, 'limit': pageLimit, 'game': game}
     streamsDict = TwitchCall(method, params)
-    count = len(streamsDict['streams'])
+    count = min(len(streamsDict['streams']), pageLimit)
 
     streams = []
     for i in range(count):
@@ -224,25 +234,6 @@ def GetStreams(pageNumber, game):
 
     return streams
 
-def PrintMenu(rows, selected, pageNum, table):
-    # Print Menu
-    Clear()
-    print Color.GREEN + table.Title + Color.END
-    print Color.BOLD + table.RowFormat.format("", *table.Headers) + Color.END
-    if len(rows) == 0:
-        print ''
-        print Color.BOLD + "NO RESULTS FOUND" + Color.END
-        print ''
-        return
-
-    for i in range(len(rows)):
-        row = rows[i]
-        if (i == selected):
-            print Color.UNDERLINE + table.RowFormat.format("", *row) + Color.END
-        else:
-            print table.RowFormat.format("", *row)
-
-    print Color.GREEN + ("Page %s" % (pageNum + 1)) + Color.END
 
 class Table:
     def __init__(self, title, headers, rowFormat):
@@ -250,20 +241,194 @@ class Table:
         self.Headers = headers
         self.RowFormat = rowFormat
 
+class LRThreads:
+    def __init__(self):
+        self.RefreshRate = 5 * 60
+        self.ShortRT = []
+        self.LongRT = threading.Timer(self.RefreshRate, self.LoadRowsLong, [])
+        self.LongRT.start()
+        self.UpdateT = threading.Timer(10, self.Update, [])
+        self.UpdateT.start()
+        self.Menu = None
+        self.Lock = threading.Lock()
+
+    def LoadRowsLong(self):
+        self.Lock.acquire()
+        curMenu = self.Menu
+        if curMenu is not None: 
+            curMenu.Rows = curMenu.ListDel(curMenu.PageNum, curMenu.Param)
+            curMenu.ts = time.time()
+
+            if curMenu is self.Menu:
+                curMenu.Print()
+        self.LongRT = threading.Timer(self.RefreshRate, self.LoadRowsLong, [])
+        self.LongRT.start()
+        self.Lock.release()
+
+    def Update(self):
+        self.Lock.acquire()
+        if self.Menu is not None:
+            self.Menu.Print()
+        self.UpdateT = threading.Timer(10, self.Update, [])
+        self.UpdateT.start()
+        self.Lock.release()
+
+    def LoadRowsShort(self):
+        curMenu = self.Menu
+        if curMenu is not None: 
+            curMenu.Rows = curMenu.ListDel(curMenu.PageNum, curMenu.Param)
+            curMenu.ts = time.time()
+
+            self.Lock.acquire()
+            if curMenu is self.Menu:
+                curMenu.Print()
+            self.Lock.release()
+
+    def SetMenu(self, menu):
+        self.Lock.acquire()
+        self.Menu = menu
+        if self.Menu is not None:
+            # Only reload rows if a significant amount of time has passed
+            curTime = time.time()
+            #print "MS Passed: %d" % ((curTime - self.Menu.ts))
+            if (curTime - self.Menu.ts) > self.RefreshRate:
+                self.Menu.Rows = []
+                shortRT = threading.Thread(target=self.LoadRowsShort)
+                self.ShortRT.append(shortRT)
+                shortRT.start()
+        self.Lock.release()
+
+    def Stop(self):
+        print "Stopping..."
+        self.Lock.acquire()
+        self.LongRT.cancel()
+        self.UpdateT.cancel()
+        self.Lock.release()
+
+        self.UpdateT.join()
+        self.LongRT.join()
+        print len(self.ShortRT)
+        i = 0
+        for thread in self.ShortRT:
+            print "Joining " + str(i)
+            thread.join()
+            i += 1
+        print "Stopped"
+        
+
+def formatTimeAgo(then):
+    hString = ""
+    mString = ""
+    now = time.time()
+    diff = now - then
+
+    hrs = diff // (60 * 60)
+    diff -= (hrs * 60 * 60)
+    mins = diff // (60)
+    diff -= (mins * 60)
+    secs = diff
+
+    
+    if hrs > 0:
+        plural = ""
+        if hrs > 1: plural = "s"
+        hString = "%d hour%s " % (hrs, plural)
+        plural = ""
+        if mins > 1: plural = "s"
+        mString = "%d minute%s " % (mins, plural)
+    elif mins > 0:
+        plural = ""
+        if mins > 1: plural = "s"
+        mString = "%d minute%s " % (mins, plural)
+
+    return hString + mString + ("%d seconds ago" % secs)
+
+
 class Menu:
-    def __init__(self, successDel, listDel, table):
+    def __init__(self, successDel, listDel, table, rows, threadedRowsLoad):
         self.SuccessDel = successDel
         self.Table = table
         self.ListDel = listDel
+        self.Rows = rows
+        self.Selected = 0
+        self.PageNum = 0
+        self.ThreadedRowsLoad = threadedRowsLoad
+        self.ts = None
+        self.Lock = threading.Lock()
+
+    def PrintBanner(self, f):
+        print Color.PURPLE + f.format("mdyyyymmo.                 /mhyyyhmhyyyhm-          .ommyyyydm        ") + Color.END
+        print Color.PURPLE + f.format("mo    mmmms::::::::::::::::om/:::om.   /m+::- .::::smmmm    om::::-   ") + Color.END
+        print Color.PURPLE + f.format("mo    +++smo+++smo+++smo+++smo+++sm.   .+++ddsdo++++++hm    -+++++hy:`") + Color.END
+        print Color.PURPLE + f.format("mo       /m.   /m.   /m.   /m.   /m.       hm+`       om          `/dh") + Color.END
+        print Color.PURPLE + f.format("mo    /++sm.   /m.   /m.   /m.   /m.   .+++dh    :++++hm    -++/    om") + Color.END
+        print Color.PURPLE + f.format("mo    mmmmm.   /m.   /m.   /m.   /m.   /mmmmh    hmmmmmm    ommm    om") + Color.END
+        print Color.PURPLE + f.format("mo    ///sm.   ./`   ./`   /m.   /m.   .///dh    :////ym    ommm    om") + Color.END
+        print Color.PURPLE + f.format("yd/`     /m.              :hm.   /ms-      hm+`       om    ommm    om") + Color.END
+        print Color.PURPLE + f.format("`:yh+++++smo+++++++++++++hmmmo+++smmmy+++++dmmdo++++++hm++++hmmm++++hm") + Color.END
+        print Color.PURPLE + f.format("   -ohmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdo-") + Color.END
+        print Color.PURPLE + f.format("      ./smmmmmdydmmmmmmmmmd/ymmmmmh:ymmmmmm-smmmmmmmo+mmmdo-mmmmh+-   \n\n") + Color.END
+
+    def Print(self):
+        self.Lock.acquire()
+        rows = self.Rows
+        selected = self.Selected
+        pageNum = self.PageNum
+        table = self.Table
+
+        (height, width) = os.popen('stty size', 'r').read().split()
+        #width = int(width) - 36
+        center = '{:^' + str(width) + '}'
+        rightAlign = '{:>' + str(width) + '}'
+
+        # Print Menu
+        Clear()
+        self.PrintBanner(center)
+        print Color.RED + center.format(table.Title) + '\n' + '\n' + Color.END
+        print Color.BOLD + center.format(table.RowFormat.format("", *table.Headers)) + Color.END
+
+        lineCount = 13 + 3 + 1
+        for i in range(len(rows)):
+            row = rows[i]
+            if (i == selected):
+                print Color.YELLOW + center.format(table.RowFormat.format("", *row)) + Color.END
+            else:
+                print center.format(table.RowFormat.format("", *row))
+            lineCount += 1
+
+        print Color.GREEN + '\n' + center.format(("Page %s" % (pageNum + 1))) + Color.END
+        lineCount += 2
+
+        for i in range(lineCount, int(height) - 3):
+            print ""
+
+        if self.ts is not None:
+            timestamp = "Last Updated: %s which was %s" % (datetime.datetime.fromtimestamp(self.ts).strftime('%m/%d %l:%M:%S %p'), formatTimeAgo(self.ts))
+            print Color.RED + rightAlign.format(timestamp) + Color.END
+
+        self.Lock.release()
+
     def Run(self, param):
+        global lrThreads
+        self.Param = param
         self.Table.Title = param
         done = False
-        selected = 0
-        pageNum = 0
-        rows = self.ListDel(pageNum, param)
+        if self.ThreadedRowsLoad:
+            if len(self.Rows) == 0:
+                Clear()
+                print "Loading..."
+                self.Rows = self.ListDel(self.PageNum, param)
+                self.ts = time.time()
+            self.Stopped = False
+            lrThreads.SetMenu(self)
+        else:
+            self.Rows = self.ListDel(self.PageNum, param)
+            self.ts = time.time()
+            lrThreads.SetMenu(None)
+
 
         while not done:
-            PrintMenu(rows, selected, pageNum, self.Table)
+            self.Print()
             
             # Get Input
             button = Input.NONE
@@ -272,32 +437,42 @@ class Menu:
 
                 if action == Action.BUTTON_DOWN:
                     if button == Input.BUTTON_A:
-                        self.SuccessDel(rows[selected][0])
+                        self.SuccessDel(self.Rows[self.Selected][0])
                         break;
                     if button == Input.BUTTON_B:
                         done = True
                         break;
                     elif button == Input.AXIS_DOWN:
-                        selected = (selected + 1) % len(rows)
+                        if len(self.Rows) > 0:
+                            self.Selected = (self.Selected + 1) % len(self.Rows)
                         break;
                     elif button == Input.AXIS_UP:
-                        selected = max(selected - 1, 0)
+                        self.Selected = max(self.Selected - 1, 0)
                         break;
                     elif button == Input.AXIS_RIGHT:
-                        if len(rows) == pageLimit:
-                            pageNum += 1
-                            rows = self.ListDel(pageNum, param)
+                        if len(self.Rows) == pageLimit:
+                            self.PageNum += 1
+                            Clear()
+                            print "Loading..."
+                            self.Rows = self.ListDel(self.PageNum, param)
+                            self.ts = time.time()
                             break;
                     elif button == Input.AXIS_LEFT:
-                        if pageNum > 0:
-                            pageNum -= 1
-                            rows = self.ListDel(pageNum, param)
+                        if self.PageNum > 0:
+                            self.PageNum -= 1
+                            Clear()
+                            print "Loading..."
+                            self.Rows = self.ListDel(self.PageNum, param)
+                            self.ts = time.time()
                             break;
                     if button == Input.BUTTON_Y:
-                        rows = self.ListDel(pageNum, param)
+                        Clear()
+                        print "Loading..."
+                        self.Rows = self.ListDel(self.PageNum, param)
+                        self.ts = time.time()
                         break;
 
-def PlayStream(stream):
+def PlayStreamWS(stream):
     Clear()
     print "Loading..."
     url = "twitch.tv/%s" % stream
@@ -308,42 +483,119 @@ def PlayStream(stream):
     while action != Action.BUTTON_DOWN or button != Input.BUTTON_B:
         action,button = ReadInput()
 
-    #p.terminate()
-    #process = psutil.Process(p.pid)
-    #child_pid = process.get_children(recursive=true)
-    #for pid in child_pid:
-    #    os.kill(pid.pid, sig)
     sig = signal.SIGTERM
+    # Kill the video player
+    KillProcessByName("omxplayer.bin", sig)
+    # kill livestreamer
     os.kill(p.pid, sig)
 
+def KillProcessByName(name, sig):
+    try:
+        omxPid = int(subprocess.check_output(["pidof", name]))
+        os.kill(omxPid, sig)
+    except subprocess.CalledProcessError:
+        print "Unable to close Omxplayer"
+
+def PlayStream(stream):
+    global stopLSThread, player
+    stopLSThread = False
+    Clear()
+    url = "twitch.tv/%s" % stream
+    print "Loading... %s" % url
+
+    player = subprocess.Popen(['omxplayer', '/tmp/stream'])
+
+    lsThread = threading.Thread(target=ReadWriteStream, args=(url,))
+    lsThread.start()
+
+    button = Input.NONE
+    action = Action.NONE
+    while action != Action.BUTTON_DOWN or button != Input.BUTTON_B:
+        action,button = ReadInput()
+
+    stopLSThread = True
+    sig = signal.SIGTERM
+    KillProcessByName("omxplayer.bin", sig)
+    os.kill(player.pid, sig)
+
+    os.unlink(pipeStreamPath)
+    MakeFIFOPipe()
+
+def ReadWriteStream(url):
+    print "LSThread Start"
+    global stopLSThread
+    stream = livestreamer.streams(url)
+    stream = stream['best']
+    fd = stream.open()
+
+    try:
+        with open("/tmp/stream", "wb") as f:
+            while not stopLSThread:
+                data = fd.read(8192)
+                f.write(data)
+    except IOError as err:
+        if err.errno in ACCEPTABLE_ERRNO:
+            print "Player closed"
+        else:
+            print "Error when writing to output: %s" % err
+
+    stopLSThread = False;
+    print "LSThread Complete"
+
+def MakeFIFOPipe():
+    if os.path.exists(pipeStreamPath):
+        return
+
+    try:
+        os.mkfifo(pipeStreamPath)
+    except OSError, e:
+        print "Failed to create FIFO: %s" % e
+        exit(0)
 
 def main():
+    global topStreams, topGames, myGames, myFollows, streamForGame, lrThreads
+    MakeFIFOPipe()
+    lrThreads = LRThreads()
+
+    try:
     
-    rowFormat = "{:<10}{:<30}{:<100}{:<15}{:<15}"
-    streamTable = Table('', ["Name", "Status", "Viewers", "Followers"], rowFormat)
-    streamMenu = Menu(PlayStream, GetStreams, streamTable)
+        # Streams per game menu
+        rowFormat = "{:<10}{:<30}{:<100}{:<15}{:<15}"
+        streamTable = Table('', ["Name", "Status", "Viewers", "Followers"], rowFormat)
+        streamMenu = Menu(PlayStream, GetStreams, streamTable, streamsForGame, True)
 
-    rowFormat = "{:<10}{:<70}{:<15}{:<15}"
-    gameTable = Table('Top Games List', ["Name", "Viewers", "Channels"], rowFormat)
-    gameMenu = Menu(streamMenu.Run, GetGames, gameTable)
+        # Top Games menu
+        rowFormat = "{:<10}{:<70}{:<15}{:<15}"
+        gameTable = Table('Top Games List', ["Name", "Viewers", "Channels"], rowFormat)
+        gameMenu = Menu(streamMenu.Run, GetGames, gameTable, topGames, True)
 
+        # Top Streams menu
+        rowFormat = "{:<10}{:<30}{:<50}{:<100}{:<15}"
+        topStreamTable = Table('', ["Name", "Game", "Status", "Viewers"], rowFormat)
+        topStreamsMenu = Menu(PlayStream, GetTopStreams, topStreamTable, topStreams, True)
 
-    rowFormat = "{:<10}{:<30}{:<50}{:<100}{:<15}"
-    topStreamTable = Table('', ["Name", "Game", "Status", "Viewers"], rowFormat)
-    topStreamsMenu = Menu(PlayStream, GetTopStreams, topStreamTable)
+        # My followed streams menu
+        followedStreamMenu = Menu(PlayStream, GetFollowedStreams, topStreamTable, myFollows, True)
 
-    followedStreamMenu = Menu(PlayStream, GetFollowedStreams, topStreamTable)
+        # MY games menu
+        rowFormat ="{:<10}{:<100}"
+        myGamesTable = Table('', ["Game"], rowFormat)
+        myGamesMenu = Menu(streamMenu.Run, GetMyGames, myGamesTable, myGames, False)
 
-    rowFormat ="{:<10}{:<30}"
-    myGamesTable = Table('', ["Game"], rowFormat)
-    myGamesMenu = Menu(streamMenu.Run, GetMyGames, myGamesTable)
+        while 1:
+            followedStreamMenu.Run('Followed Stream Menu')
+            myGamesMenu.Run('My Games')
+            gameMenu.Run('Top Games List')
+            topStreamsMenu.Run('Top Streams List')
+        return
 
-    while 1:
-        followedStreamMenu.Run('Followed Stream Menu')
-        myGamesMenu.Run('My Games')
-        gameMenu.Run('Top Games List')
-        topStreamsMenu.Run('Top Streams List')
-    return
+    except KeyboardInterrupt:
+        print "Closing..."
+    finally:
+        lrThreads.Stop()
+        pipe.close()
+        os.unlink(pipeStreamPath)
+        os.system('stty sane')
 
 if __name__ == "__main__":
     main()
